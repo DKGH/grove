25b63f9da93ef4471926964c7f80ad0698f1cafd
Dheeraj Kumar <dheeraj.iitm@gmail.com>
2026-05-18 20:27:44 -0700

    Initial commit

---
+++ headers/BinaryTree.hxx
@@ -0,0 +1,161 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+namespace Grove
+{
+    template <typename T>
+    struct Node
+    {
+        T data;
+        Node<T> *left{nullptr};
+        Node<T> *right{nullptr};
+
+#pragma region Breadth-First Traversal
+
+        vector<T> GetLevelOrder() const
+        {
+            vector<T> result;
+            vector<const Node<T> *> queue{this};
+            while (!queue.empty())
+            {
+                const Node<T> *current = queue.front();
+                queue.erase(queue.begin());
+                result.push_back(current->data);
+                if (current->left)
+                    queue.push_back(current->left);
+                if (current->right)
+                    queue.push_back(current->right);
+            }
+            return result;
+        }
+
+#pragma endregion
+
+#pragma region Depth-First Traversals
+
+        vector<T> GetPreOrder() const
+        {
+            vector<T> result;
+            result.push_back(data);
+            if (left)
+            {
+                const auto &leftValues = left->GetPreOrder();
+                result.insert(result.end(), leftValues.begin(), leftValues.end());
+            }
+            if (right)
+            {
+                const auto &rightValues = right->GetPreOrder();
+                result.insert(result.end(), rightValues.begin(), rightValues.end());
+            }
+            return result;
+        }
+
+        vector<T> GetInOrder() const
+        {
+            vector<T> result;
+            if (left)
+            {
+                const auto &leftValues = left->GetInOrder();
+                result.insert(result.end(), leftValues.begin(), leftValues.end());
+            }
+            result.push_back(data);
+            if (right)
+            {
+                const auto &rightValues = right->GetInOrder();
+                result.insert(result.end(), rightValues.begin(), rightValues.end());
+            }
+            return result;
+        }
+
+        vector<T> GetPostOrder() const
+        {
+            vector<T> result;
+            if (left)
+            {
+                const auto &leftValues = left->GetPostOrder();
+                result.insert(result.end(), leftValues.begin(), leftValues.end());
+            }
+            if (right)
+            {
+                const auto &rightValues = right->GetPostOrder();
+                result.insert(result.end(), rightValues.begin(), rightValues.end());
+            }
+            result.push_back(data);
+            return result;
+        }
+
+#pragma endregion
+
+#pragma region Visitors
+
+        void VisitLevelOrder(const function<void(const T &)> &visitor) const
+        {
+            vector<const Node<T> *> queue{this};
+            while (!queue.empty())
+            {
+                const Node<T> *current = queue.front();
+                queue.erase(queue.begin());
+                visitor(current->data);
+                if (current->left)
+                    queue.push_back(current->left);
+                if (current->right)
+                    queue.push_back(current->right);
+            }
+        }
+
+        void VisitPreOrder(const function<void(const T &)> &visitor) const
+        {
+            visitor(data);
+            if (left)
+                left->VisitPreOrder(visitor);
+            if (right)
+                right->VisitPreOrder(visitor);
+        }
+
+        void VisitInOrder(const function<void(const T &)> &visitor) const
+        {
+            if (left)
+                left->VisitInOrder(visitor);
+            visitor(data);
+            if (right)
+                right->VisitInOrder(visitor);
+        }
+
+        void VisitPostOrder(const function<void(const T &)> &visitor) const
+        {
+            if (left)
+                left->VisitPostOrder(visitor);
+            if (right)
+                right->VisitPostOrder(visitor);
+            visitor(data);
+        }
+
+#pragma endregion
+    };
+
+    template <typename T>
+    Node<T> *GenerateBinaryTree(const vector<T> &elements)
+    {
+        const size_t n{elements.size()};
+        if (n == 0)
+            return nullptr;
+        vector<Node<T> *> nodes;
+        nodes.reserve(elements.size());
+        for (const auto &element : elements)
+            nodes.push_back(new Node<T>{element, nullptr, nullptr});
+        for (size_t i{0}; i < n; ++i)
+        {
+            if (const size_t leftIndex = 2 * i + 1; leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (const size_t rightIndex = 2 * i + 2; rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+
+        return nodes[0];
+    }
+}

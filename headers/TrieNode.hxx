12afffe19f25a80629f4c6fb45eca64638540b17
Dheeraj Kumar <dheeraj.iitm@gmail.com>
2026-05-29 17:33:06 -0700

    Add support for Fenwick trees

--- headers/TrieNode.hxx
+++ headers/TrieNode.hxx
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <unordered_map>
 
 using namespace std;
@@ -10,6 +11,13 @@ namespace Grove
   struct TrieNode final
   {
     unordered_map<T, TrieNode<T>> Children;
+    bool IsEndOfWord{false};
+  };
+
+  template <typename T, size_t N>
+  struct FixedTrieNode final
+  {
+    array<TrieNode<T>, N> Children;
     bool IsEndOfWord{false};
   };
 }
